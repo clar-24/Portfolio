88e69d977f66e9fb92e049fcea89cda124472776
Clarence Lauron <lauronclarencesenoc@gmail.com>
2025-01-14 04:03:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-// Select the button
 const getStartedButton = document.getElementById('get-started-btn');
 
-// Add event listener for button click
 getStartedButton.addEventListener('click', () => {
-    // Scroll to the About section
     document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
-
-    // Alternatively, display a message
     console.log('Get Started button clicked!');
 });
